92ed20edfbca026a8e826cecdfc361795491139c
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2023-09-02 04:13:38 +0700

    Update UI

--- src/components/Weather/WeatherItem/WeatherItem.jsx
+++ src/components/Weather/WeatherItem/WeatherItem.jsx
@@ -3,18 +3,34 @@ import React from "react";
 import "./WeatherItem.css";
 import Card from "../../UI/Card";
 import Rain from "../../../assets/images/rain.png";
+import Humidity from "../../../assets/images/humidity.png";
+import Wind from "../../../assets/images/wind.png";
 
 const WeatherItem = (props) => {
   return (
-    <Card className="weather-item">
-      <div>
-        <img src={Rain} alt="Rain" className="weather-icon" />
+    <Card>
+      <div className="weather-item">
+        <div className="weather-icon">
+          <img src={Rain} alt="Rain" />
+        </div>
         <div className="temp temp-detail">22</div>
         <div className="city temp-detail">New york</div>
+
         <div className="info-detail">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
+          <div className="col">
+            <img src={Humidity} alt="Humidity" className="humidity-img" />
+            <div>
+              <div className="humidity">50%</div>
+              <div>Humidity</div>
+            </div>
+          </div>
+          <div className="col">
+            <img src={Wind} alt="Wind" className="wind-img" />
+            <div>
+              <div className="humidity">1.85 km/h</div>
+              <div>Wind Speed</div>
+            </div>
+          </div>
         </div>
       </div>
     </Card>
