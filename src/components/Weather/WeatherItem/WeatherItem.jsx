04878d29f93c605e7ffcdc7eea3835327d22661f
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2023-09-02 03:18:58 +0700

    Add UI Card Weather

--- src/components/Weather/WeatherItem/WeatherItem.jsx
+++ src/components/Weather/WeatherItem/WeatherItem.jsx
@@ -2,14 +2,22 @@ import React from "react";
 
 import "./WeatherItem.css";
 import Card from "../../UI/Card";
+import Rain from "../../../assets/images/rain.png";
 
 const WeatherItem = (props) => {
   return (
-    <div>
-      <Card className="weather-item">
-        <div>Hi</div>
-      </Card>
-    </div>
+    <Card className="weather-item">
+      <div>
+        <img src={Rain} alt="Rain" className="weather-icon" />
+        <div className="temp temp-detail">22</div>
+        <div className="city temp-detail">New york</div>
+        <div className="info-detail">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
+    </Card>
   );
 };
 
