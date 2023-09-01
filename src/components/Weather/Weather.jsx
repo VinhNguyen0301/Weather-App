efd749677fed239be50ef59c427945cb073005ab
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2023-09-02 06:12:46 +0700

    Call api, binding data

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Card from "../UI/Card";
 import WeatherItem from "./WeatherItem/WeatherItem";
 
 import "./Weather.css";
+import Search from "./Search/Search";
 
 const Weather = (props) => {
+  const [weatherData, setWeatherData] = useState({})
+  const apiKey = "5c8a89b7f3f21acf674f93dd5ac8d9a7";
+  const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=sydney";
+
+  const checkWeather = async () => {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+    setWeatherData(data);
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    checkWeather();
+  }, []);
+
   return (
     <div>
       <Card className="weather">
-        <WeatherItem />
+        <div className="search-component">
+          <Search />
+        </div>
+        <WeatherItem weatherData={weatherData}/>
       </Card>
     </div>
   );
--- src/components/Weather/WeatherItem/WeatherItem.jsx
+++ src/components/Weather/WeatherItem/WeatherItem.jsx
@@ -5,34 +5,31 @@ import Card from "../../UI/Card";
 import Rain from "../../../assets/images/rain.png";
 import Humidity from "../../../assets/images/humidity.png";
 import Wind from "../../../assets/images/wind.png";
-import Search from "../Search/Search";
 
-const WeatherItem = (props) => {
+const WeatherItem = ({weatherData}) => {
+  
   return (
     <Card>
       <div className="weather-item">
-        <div className="search-component">
-        <Search />
-
-        </div>
+      
         <div className="weather-icon">
           <img src={Rain} alt="Rain" />
         </div>
-        <div className="temp temp-detail">22</div>
-        <div className="city temp-detail">New york</div>
+        <div className="temp temp-detail">{weatherData?.main?.temp}</div>
+        <div className="city temp-detail">{weatherData?.name}</div>
 
         <div className="info-detail">
           <div className="col">
             <img src={Humidity} alt="Humidity" className="humidity-img" />
             <div>
-              <div className="humidity">50%</div>
+              <div className="humidity">{weatherData?.main?.humidity}%</div>
               <div>Humidity</div>
             </div>
           </div>
           <div className="col">
             <img src={Wind} alt="Wind" className="wind-img" />
             <div>
-              <div className="humidity">1.85 km/h</div>
+              <div className="humidity">{weatherData?.wind?.speed} km/h</div>
               <div>Wind Speed</div>
             </div>
           </div>
